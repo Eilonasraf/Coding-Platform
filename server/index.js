d0fb6e3d3803f6fb9c295887bdfdcdcdd5410d5f
איילון אסרף <eilon@yylwns-MacBook-Pro.local>
2025-06-21 22:24:04 +0300

    check

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+// ONLY CORS, no routers or sockets yet
+const corsOptions = {
+  origin: process.env.CLIENT_ORIGIN,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  credentials: true
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.get('/', (req, res) => res.send('OK'));
 
